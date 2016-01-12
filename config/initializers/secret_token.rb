0aa2ebbd47c1c78c13737c0a9cb811f48fddda03
Revathi <revathi.software1@gmail.com>
2016-01-11 20:49:27 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apple::Application.config.secret_token = 'af22aa1f8824de8dc0c83460714ceb3a5a49b7643db686305b540f164dbb4f15bf1f337194e64001b41620e44ea6fcad518b384b763b8e573567d0981cae1a40'
